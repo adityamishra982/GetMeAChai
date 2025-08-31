0beaf7cab206fcd3f813220b1384d4a143c5a57e
Aditya Mishra <aditya.mishraa209@gmail.com>
2025-08-31 12:46:05 +0530

    eslint changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['media.tenor.com', 'cdnl.iconscout.com', 'www.arrowheadgrp.com', 'c10.patreonusercontent.com', ],
     },
+    eslint:{
+        ignoreDuringBuilds: true,
+    }
 };
 
 export default nextConfig;
